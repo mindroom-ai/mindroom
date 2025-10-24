2439412ec7902a11bea6eae07d97eead931b4ae9
Bas Nijholt <bas@nijho.lt>
2025-10-23 20:38:15 -0700

    simplify: single TLS secret and rely on wildcard for www\n\n- Use one cert secret (platform-tls-cert) for apps + apex/www; cert-manager will unify SANs\n- Drop explicit Porkbun www records; wildcard A/AAAA already cover it\n- Keep minimal, dedicated apex redirect ingress to avoid global redirect annotations on app/api/webhooks\n

--- cluster/terraform/terraform-k8s/modules/dns/dns.tf
+++ cluster/terraform/terraform-k8s/modules/dns/dns.tf
@@ -33,7 +33,7 @@ locals {
   # "mindroom.chat" - "mindroom.chat" = ""
   subdomain_prefix = var.superdomain == var.root_domain ? "" : trimspace(trimsuffix(var.superdomain, ".${var.root_domain}"))
 
-  platform_subdomains = ["app", "api", "webhooks", "www"]
+  platform_subdomains = ["app", "api", "webhooks"]
 }
 
 resource "porkbun_dns_record" "platform_a" {
